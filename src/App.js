124e51fdca44dd9de7f72b1f65c785bbf82dc4e4
SamSmith21 <sammy_smith_@hotmail.com>
2022-01-23 12:08:11 +0000

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,5 +3,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <p>
+          This page is coded by
+          <a
+            href="https://www.linkedin.com/in/samantha-smith-5014424b/"
+            target="_blank"
+          >
+            Samantha Smith
+          </a>
+          and is
+          <a
+            href="https://github.com/SamSmith21/react-weather-app"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
