06968984072b4e1a8a7702bcf0f6298c247b56de
SamSmith21 <sammy_smith_@hotmail.com>
2022-02-08 14:13:46 +0000

    Link API to 1 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import WeatherIcon from "./WeatherIcon";
+import Axios from "axios";
+import { buildTimeValue } from "@testing-library/user-event/dist/utils";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19</span>
-            <span className="WeatherForecast-temperature-min">10</span>
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "6ae4e841169165189c06e3418b098390";
+    let lon = props.coordinates.lon;
+    let lat = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    Axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,9 +15,9 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row">
         <div className="col-6">
-          <div className="clearfix">
+          <div className="d-flex">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
             <WeatherTemperature celsius={props.data.temperature} />
